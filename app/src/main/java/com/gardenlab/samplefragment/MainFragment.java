7166b2856023664919efcc85e427b42afa40484a
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-29 12:59:18 +0900

    Change Fragment

--- app/src/main/java/com/gardenlab/samplefragment/MainFragment.java
+++ app/src/main/java/com/gardenlab/samplefragment/MainFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainFragment extends Fragment {
 
@@ -16,7 +17,18 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button button = rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity activity = (MainActivity) getActivity();
+                activity.onFragmentChanged(0);
+            }
+        });
+
+        return rootView;
     }
 
 
